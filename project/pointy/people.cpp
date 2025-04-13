b2d9c770746b26745ec075caf916bc9340475342
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-13 21:03:05 +0330

    Chore

---
+++ project/pointy/people.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using std::cout, std::cin, std::endl, std::string;
+
+struct students {
+    string name;
+    int id;
+    float grade_avg;
+};
+
+int main()
+{
+
+    int n;
+    int *ptr;
+
+
+    cout << endl;
+    return 0;
+}
