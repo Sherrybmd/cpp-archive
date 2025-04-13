a2d4e0cb3f1b78bee26c60345f16d5d91b6f089e
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-13 21:03:40 +0330

    Chore, studies

---
+++ replace/replace.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+
+    name.replace(startingpos, howmanychars, whattoreplacewithasstr);
+
+    cout << endl;
+    return 0;
+}
