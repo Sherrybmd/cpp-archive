7bd2a910b1eb89f11ace83225f609c6116c14827
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-14 15:44:11 +0330

    FUCKED UP x.name no longer shows shit

--- project/pointy/people.cpp
+++ project/pointy/people.cpp
@@ -6,32 +6,40 @@ struct students {
     string name;
     int id;
     float grade_avg;
-}x, *ptr;
+}x,*ptr;
 
 void set_student(string *, int *, float *);
 
 int main()
 {
-    ptr = &x;
+    students *ptr = new students;  //weewoo weewoo !!
+
     cout << ptr << "\n";
 
     bool stop = false;
     while (stop == false)
     {
         cout << "enter thy name:" << "\n>";
-
         cin >> ptr -> name;
 
-        cout << ptr -> name << "\n";
+        cout << "enter thy number (please):" << "\n>";
+        cin >> ptr -> id;
+
+        cout << "enter thy grade:" << "\n>";
+        cin >> ptr -> grade_avg;
+
 
         stop = true;
     }
 
-    cout << x.name << endl;
+    cout << "your name is: " << ptr -> name << "\n"
+         << "your id is: " << ptr -> id << "\n"
+         << "your grade is: " << ptr -> grade_avg << "\n"
+         << endl;
 
     cout << ptr << "\n";
 
-    delete[ ] ptr;
+    delete ptr;
 
     cout << endl;
     return 0;
