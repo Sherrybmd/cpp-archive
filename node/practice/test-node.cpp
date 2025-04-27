de34dfaf158c0a235d8982de8c318de9e79a8904
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-04-27 19:13:27 +0330

    Add useful constructor with address

---
+++ node/practice/test-node.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::string, std::cout, std::cin;
+
+
+struct Node {
+
+    int data;
+    Node* ptr;
+};
+
+int main()
+{
+
+
+    cout << "\n";
+    return 0;
+}
---
+++ stack/04/constructor-new.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+class MyClass
+{
+
+    int x;
+    int y;
+public:
+    MyClass() : x(0), y(0) {
+
+        cout << "Default Constructor" << "\n";
+    }
+
+    // parametr creator
+    MyClass(int a, int b) : x(a), y(b) {
+        cout << "Parametrized Constructor" << "\n";
+    }
+
+
+    MyClass(const MyClass &obj) : x(obj.x), y(obj.y) {
+        cout << "Copy Coonstructor" << "\n";
+    }
+
+    void display() const {
+        cout << "x: " << x << ", y: " << y << "\n";
+    }
+};
+
+int main()
+{
+    MyClass obj1;          // default constructor
+    MyClass obj2(10, 20);  // parametr constructor
+    MyClass obj3 = obj2;   // copy constructor
+
+    obj1.display();
+    obj2.display();
+    obj3.display();
+
+    cout << "\n";
+    return 0;
+}
