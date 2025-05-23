112cf71bac11bdb4bccfe4b1c8f26e185a717687
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-05-23 14:18:45 +0330

    better template, 2 variable types

--- testground/review/Templates/template.cpp
+++ testground/review/Templates/template.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 using namespace std;
-template <typename T>
+template <typename T, typename U>
 
-T func(T data1, T data2)
+auto func(T data1, U data2)
 {
     return data1 + data2;
 }
 
 int main()
 {
-    cout << func(5, 3) << "\n";
+    cout << func(5, 3.5) << "\n";
 
 
     cout << "\n";
